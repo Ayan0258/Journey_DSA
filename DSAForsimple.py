9fd0ca82e77d00f3472fb5d23859466db293976c
Ayan <ayandasanu666666@gmail.com>
2026-03-18 21:29:23 +0530

    today dsa homework

--- DSAForsimple.py
+++ DSAForsimple.py
@@ -1,8 +1,59 @@
+# """
+
+# This the start of the dsa journey
+
+
+
+# """
+
+# print("Cheking Github")
+
+
+
+# """
+# q. Is the number is odd or even.
+
+# """
+
+# n = int(input("Enter your Number : "))
+
+# if (n%2==0):
+#     print("even")
+
+# else:
+#     print("odd")
+
+
+
+# """
+# q. Sum of numbers from 1 to N
+
+# """
+
+# n = int (input("Enter your number: "))
+# sum = 0
+# for i in range(1, n+1):
+#     sum = sum + i
+
+# print(sum)
+
+
+
 """
 
-This the start of the dsa journey
-
+q. Is number prime or not.
 
 """
+n = int(input("Enter your number: "))
 
-print("Cheking Github")
+is_prime = True
+
+for i in range(2, n):
+    if n % i == 0:
+        is_prime = False
+        break
+
+if is_prime:
+    print("Its a prime number")
+else:
+    print("Its not a prime number")
---
+++ Dsaallhomeworks.py
@@ -0,0 +1,58 @@
+"""
+q. Calculate "simple interest" from principle (P),Rate (R) & Time(T).
+
+SI = (P x R x T)/100
+
+q. Calculate Max of 2 numbers.
+
+q. Calculate factorial of a number N
+q. Given a person age, find if they should get a driving liccense or not
+"""
+
+# P = float(input("Enter your principal: "))
+# R = float(input("Enter rate: "))
+# T = float(input('Enter the time: '))
+
+# SI = (P * R * T)/100
+
+# print(SI)
+
+#///////// next question
+
+# a =int(input("Enter your number: "))
+# b = int (input("Enter your number: "))
+
+# if (a>b):
+#     print("Your number is greater than b")
+
+# elif (b<a):
+#     print("your number is smaller than a")
+
+# else:
+#     print("Your number are same as a and b")
+
+# /////this is next question
+
+# n = int(input("Enter your number: "))
+
+# fact = 1
+
+# for i in range(1, n+1):
+#     fact = fact * i
+
+# print("Factorial =", fact)
+
+# ///this is next question
+
+
+# n =int(input("Enter your age: "))
+
+
+# if (n >= 18):
+#     print("You are eligible for driving license")
+
+# else:
+#     print("You are too young to get driving license")
+
+
+# DONE.......................
