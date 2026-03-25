3ce23bf11270273116e7d029c1fe374c98a3b4b0
Ayan Das <ayandasanu666666@gmail.com>
2026-03-25 21:59:50 +0530

    New problem

--- DSa.newtopic.py
+++ DSa.newtopic.py
@@ -1,9 +1,43 @@
 # ///gcd//HCF///
 
 
-def gcd(a,b):
-    if b==0:
-        return a
-    return gcd(b,a%b)
+# def gcd(a,b):
+#     if b==0:
+#         return a
+#     return gcd(b,a%b)
 
-print(gcd(15,125))
+# print(gcd(15,125))
+
+# //lcm//
+
+# def gcd(a,b):
+#     if b==0:
+#         return a
+#     return gcd(b,a%b)
+
+# def lcm(a,b):
+#     return a*b//(gcd(b,a%b))
+
+
+# print(gcd(15,50))
+# print(lcm(15,50))
+
+"""
+Power of (x,n)
+
+"""
+
+def thepowerof(x, n):
+    if n == 0:
+        return 1
+    half = thepowerof(x, n //2 )
+
+    if n % 2 == 0:
+        return half * half
+    else:
+        return x * half * half
+    
+x = int(input("Enter base: "))
+n = int(input("Enter power: "))
+
+print(thepowerof(x,n))
